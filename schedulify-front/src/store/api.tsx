5cbad9e9302cce0f7aa94a466deba31a4cf6b8db
farnsworth <farnsworth3010@proton.me>
2023-11-12 20:36:21 +0000

    fix: docker fix

--- schedulify-front/src/store/api.tsx
+++ schedulify-front/src/store/api.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const scheduleApi = createApi({
   reducerPath: 'scheduleApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8585/api.php' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://79.133.181.113:8585/api.php' }),
   endpoints: (builder) => ({
     getSchedule: builder.query({
       query: (id) => `?group_id=${id}`,
